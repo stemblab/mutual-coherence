b82b9bf78f2e0fe6e7639c5fc3618ff5669e8d2e
garyballantyne <gary.ballantyne@haulashore.com>
2014-09-14 07:52:59 +1200

    Tidy.

--- matlab/mcoh.m
+++ matlab/mcoh.m
@@ -4,7 +4,5 @@ M = size(A,2); % number of rows in $A$
 An = normc(A); % normalize columns of $A$ to find $A_n$
 cloak=eye(M)~=1; % ones everywhere but on the diagonal
 mu=max(max(abs(An'*An).*cloak)); % max off-diagonal term of $A^T_n A_n$
-%
 
-%!end (6)
 
